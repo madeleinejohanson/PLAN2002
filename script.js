dd803e8fc2f16cc8990d0e4a7d11c9da60dfe248
madeleinejohanson <madeleine.johanson53@gmail.com>
2017-05-25 04:16:20 +1000

    update

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFkZWxlaW5lam9oYW5zb24iLCJhIjoiY2lzczduYzJ4MDZrODJucGh0Mm1xbmVxNCJ9.i7q4iT8FFgh_y5v4we5UhQ';
 var map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/light-v9',
-    center: [151.004105, -33.813002],
+    center: [151.104433, -33.918212],
 
     // homebush [151.08223, -33.8680],
     // sydney pier [151.2056, -33.8572],
-    zoom: 18,
-    pitch: 45,
-    bearing: -17.6,
+    zoom: 12,
+    pitch: 15,
+    bearing: 0,
     container: 'map'
 });
 map.on('load', function() {
 
     map.addSource('json-buildings', {
                     'type': 'geojson',
-                    'data': './GeoJSON/tower_1.GeoJSON'
+                    'data': './GeoJSON/bus_train_1.GeoJSON'
                 }),
 
     map.addLayer({
